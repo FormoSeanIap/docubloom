a689d1d8c444b7416efc007f86a8d5a008c60f7d
FormoSeanIap <lf3.net@gmail.com>
2022-04-24 17:10:15 +0800

    UPDATE: remove authorization from mockserver routes

--- server/routes/mockServer_route.js
+++ server/routes/mockServer_route.js
@@ -1,17 +1,13 @@
 import express from 'express';
 
-import { asyncHandler, authentication, authorizationDoc } from '../../utils/util.js';
+import { asyncHandler } from '../../utils/util.js';
 import { getExample, getExampleFromExamples } from '../controllers/mockServer_controller.js';
-
-import { DOC_ROLE } from '../../utils/constants.js';
 
 const router = express.Router();
 
-router.route('/docs/:docId/example')
-  .get(authentication(), authorizationDoc(DOC_ROLE.VIEWER), asyncHandler(getExample));
+router.route('/docs/:docId/example').get(asyncHandler(getExample));
 
-router.route('/docs/:docId/examples')
-  .get(authentication(), authorizationDoc(DOC_ROLE.VIEWER), asyncHandler(getExampleFromExamples));
+router.route('/docs/:docId/examples').get(asyncHandler(getExampleFromExamples));
 
 export { router };
 
